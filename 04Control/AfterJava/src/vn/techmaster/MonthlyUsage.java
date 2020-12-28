dd1417d29162a5e0973d1e2bddcd92def1b291ce
tienthanhpham <tienthanhpham@gmail.com>
2020-12-28 15:14:41 +0700

    JavaSwitchRefactoring

--- 04Control/AfterJava/src/vn/techmaster/MonthlyUsage.java
+++ 04Control/AfterJava/src/vn/techmaster/MonthlyUsage.java
@@ -11,21 +11,4 @@ public class MonthlyUsage {
         SmsCount = smsCount;
     }
 
-    public void Generate(MonthlyStatement monthlyStatement) throws Exception {
-        switch (Customer.Type)
-        {
-            case PayAsYouGo:
-                monthlyStatement.CallCost = 0.12f * CallMinutes;
-                monthlyStatement.SmsCost = 0.12f * SmsCount;
-                monthlyStatement.TotalCost = monthlyStatement.CallCost + monthlyStatement.SmsCost;
-                break;
-
-            case Unlimited:
-                monthlyStatement.TotalCost = 54.90f;
-                break;
-
-            default:
-                throw new Exception("The current customer type is not supported");
-        }
-    }
 }
